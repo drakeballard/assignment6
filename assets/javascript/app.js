2446477c2b06220fb3d8fb1f8b73b090d0e93dfd
Drake Ballard <drakeballard@gmail.com>
2016-12-13 01:04:13 -0600

    adding only 10 gifs per call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,5 +20,5 @@ function populateButtons(searchArray, classToAdd, areaToAddTo) {
 
 $(document).on('click','.searchButton', function() {
   var type = $(this).data('type');
-  console.log(type);
+  var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+type+'$api_key=dc6zaTOxFJmzC&limit=10';
 })
