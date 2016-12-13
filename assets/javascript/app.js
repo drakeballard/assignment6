d92e24a0e4554099d9745e1143a69ac3e1ac7ddb
Drake Ballard <drakeballard@gmail.com>
2016-12-13 00:52:10 -0600

    working on jquery for submission area

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,18 @@
 // alert("Welcome to the NFL Trivia Challenge");
+$(function() {
+  console.log("page loaded");
+})
+
+
+var searchArray = ["South Park", "Bob's Burger", "Archer", "Family Guy", " The Simpsons", "Futurama", "King of the Hill", "American Dad"];
+
+function populateButtons(searchArray, classToAddm areaToAddTo) {
+  $(areaToAddTo).empty();
+  for(var i=o; i<searchArray.length;i++) {
+    var a = $('<button>');
+    a.addclass(classToAdd);
+    a.attr('data-type',searchArray[i]);
+    a.text(searchArray[i]);
+    $(areaToAddTo).append(a);
+  }
+}
